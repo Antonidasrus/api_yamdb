deda1fb0dbae475b2f279f3a152be98eb241348b
Ксения Геращенко <qsenich@mail.ru>
2023-09-21 16:46:33 +0300

    View и эндпоинты для Category, Title, Genre

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,17 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from api.views import get_token, register, ReviewViewSet, CommentViewSet
 
+from api import views
 
 router = DefaultRouter()
-router.register(r'reviews', ReviewViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'genres', views.GenreViewSet)
+router.register(r'titles', views.TitleViewSet)
+router.register(r'reviews', views.ReviewViewSet)
+router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/signup/', register, name='register'),
-    path('v1/auth/token/', get_token, name='token')
+    path('v1/auth/signup/', views.register, name='register'),
+    path('v1/auth/token/', views.get_token, name='token')
 ]
